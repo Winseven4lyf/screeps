9cebf4d3158b33d6a0701d7326a76c5ace7a22a0
Quinn Johnson <winseven4lyf@gmail.com>
2018-11-23 12:07:49 +1000

    tower control added

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 const TRAVELER = require("Traveler");
 const ROLE = require("role_main");
+const TOWER = require("tower_main");
 
 module.exports = {
     loop: function () {
         ROLE.loop(Game.creeps);
+        TOWER.loop();
     }
 };
---
+++ src/tower_main.js
@@ -0,0 +1,51 @@
+module.exports = {
+    loop: () => {
+        if (_.find(Game.structures, { structureType: STRUCTURE_TOWER })) {
+            _.forEach(Game.rooms, room => {
+                let towers = room.find(FIND_MY_STRUCTURES,
+                    { filter: { structureType: STRUCTURE_TOWER } }
+                );
+
+                // Attack hostile creeps
+                let hostiles = room.find(FIND_HOSTILE_CREEPS);
+                if (hostiles.length > 0) {
+                    _.forEach(towers, tower => {
+                        tower.attack(_.sample(hostiles));
+                    });
+                } else {
+                    // Heal owned creeps
+                    let hurt = _.filter(room.find(FIND_MY_CREEPS),
+                        creep => creep.hits < creep.hitsMax
+                    );
+                    if (hurt.length > 0) {
+                        _.forEach(towers, tower => {
+                            tower.heal(_.sample(hurt));
+                        });
+                    } else {
+                        // Repair owned structures
+                        let damaged = _.filter(room.find(FIND_MY_STRUCTURES),
+                            structure => structure.hits < structure.hitsMax
+                        );
+                        if (damaged.length > 0) {
+                            _.forEach(towers, tower => {
+                                tower.repair(_.sample(damaged));
+                            });
+                        } else {
+                            // Repair walls
+                            let damagedWalls = _.filter(room.find(FIND_STRUCTURES,
+                                { filter: { structureType: STRUCTURE_WALL } }),
+                                wall => wall.hits < wall.hitsMax
+                            );
+                            if (damagedWalls.length > 0) {
+                                _.sortBy(damagedWalls, "hitsMax");
+                                _.forEach(towers, tower => {
+                                    tower.repair(_.first(damagedWalls));
+                                });
+                            }
+                        }
+                    }
+                }
+            });
+        }
+    }
+};
